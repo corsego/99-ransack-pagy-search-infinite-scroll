959473855a711736a80386c1633c9c357a428718
yshmarov <yshmarov@gmail.com>
2022-12-05 18:37:46 +0100

    create fake users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.all.order(created_at: :asc)
   end
 
   # GET /users/1 or /users/1.json
